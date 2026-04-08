756b5048d3c90c60e6b949263d0f63222d06347c
Eduardo Santos <1000eduardos@gmail.com>
2026-04-08 19:25:29 -0300

    :recycle: refactor: Allow browser or Node execution

--- Beginner Projects/Bin2Dec/script.js
+++ Beginner Projects/Bin2Dec/script.js
@@ -1,19 +1,21 @@
-const inputField = document.getElementById('binary');
 let binaryValue = "";
-
-if (inputField) {
-    inputField.addEventListener('input', (event) => {
-        const current = event.data;
-
-        if (current == null || current == '0' || current == '1')
-            binaryValue = inputField.value;
-        else {
-            notifyUser();
-            inputField.value = binaryValue;
-        }
-    })
-
-}
 
 const notifyUser = () => alert(`Digite somente 0's ou 1's`);
 
+const init = () => {
+    const inputField = document.getElementById('binary');
+    inputField.addEventListener('input', (event) => {
+        const current = event.data;
+
+        if (current == null || current === '0' || current === '1') {
+            binaryValue = inputField.value;
+        } else {
+            notifyUser();
+            inputField.value = binaryValue;
+        }
+    });
+};
+// 👉 Se estiver no navegador, chama init automaticamente
+if (typeof window !== 'undefined') window.addEventListener('DOMContentLoaded', init);
+// 👉 Se estiver em Node (Jest), exporta para testes
+if (typeof module !== 'undefined' && module.exports) module.exports = { notifyUser, init };
