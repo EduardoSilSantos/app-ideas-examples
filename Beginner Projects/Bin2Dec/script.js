88f2c37960e608973815c432a0b43c0cb247068a
Eduardo Santos <1000eduardos@gmail.com>
2026-04-09 16:50:07 -0300

    :sparkles: feat: Add toDecimal function to convert binary to decimal

--- Beginner Projects/Bin2Dec/script.js
+++ Beginner Projects/Bin2Dec/script.js
@@ -17,14 +17,24 @@ const init = () => {
     converter();
 };
 const converter = () => {
+    const outputField = document.getElementById('decimal');
     const form = document.getElementById('formConverter');
     form.addEventListener('submit', (event) => {
         event.preventDefault();
-        alert('Funcionalidade em desenvolvimento!');
+        outputField.value = toDecimal(binaryValue);
     })
 
 };
+const toDecimal = (binary) => {
+    let exp = 0;
+    let result = 0;
+    for (let i = binary.length - 1; i >= 0; i--) {
+        if (binary[i] == 1) result += 2 ** exp;
+        exp++;
+    }
+    return result;
+}
 // 👉 Se estiver no navegador, chama init automaticamente
 if (typeof window !== 'undefined') window.addEventListener('DOMContentLoaded', init);
 // 👉 Se estiver em Node (Jest), exporta para testes
-if (typeof module !== 'undefined' && module.exports) module.exports = { notifyUser, init };
+if (typeof module !== 'undefined' && module.exports) module.exports = { notifyUser, init, toDecimal };
